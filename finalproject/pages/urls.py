739c55be8544bc324232bc6f2466e78862e57943
FacundoTogliefoso <facundo.togliefoso@gmail.com>
2021-06-07 00:25:35 -0300

    Add delete view

--- finalproject/pages/urls.py
+++ finalproject/pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PageCreateView, PageUpdateView, PagesListView, PageDetailView
+from .views import PageCreateView, PageDeleteView, PageUpdateView, PagesListView, PageDetailView
 
 pages_patterns = ([
     path('', PagesListView.as_view(), name='pages'),
     path('<int:pk>/', PageDetailView.as_view(), name='page'),
     path('update/<int:pk>/', PageUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', PageDeleteView.as_view(), name='delete'),
     path('create/', PageCreateView.as_view(), name='create'),
 ], 'pages')
--- finalproject/pages/views.py
+++ finalproject/pages/views.py
@@ -1,8 +1,9 @@
 # from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.urls.base import reverse
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
@@ -27,6 +28,11 @@ class PageUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+
+class PageDeleteView(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
 
 # def pages(request):
 #     pages = get_list_or_404(Page)
