01691497f10f1a50e166f6654ba68b2f070fcd94
FacundoTogliefoso <facundo.togliefoso@gmail.com>
2021-06-08 02:24:09 -0300

    Refactor pages form view and model

--- finalproject/pages/forms.py
+++ finalproject/pages/forms.py
@@ -5,12 +5,11 @@ class PageForm(forms.ModelForm):
     class Meta:
 
         model = Page
-        fields = ['title', 'content','order']
+        fields = ['title', 'content', 'image', 'posted','order']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
             'order': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Orden'}),
-        }
-        labels = {
-            'title': '', 'content': '', 
+            'image': forms.ClearableFileInput(attrs={'class':'form-control-file mt-3'}),
+            'posted': forms.DateTimeInput(attrs={'class': 'datepicker'}),
         }
