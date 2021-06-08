df356c2c16311353d5fcf6418ee55b9136bc8815
FacundoTogliefoso <facundo.togliefoso@gmail.com>
2021-06-08 01:08:25 -0300

    Refactor models forms views annd template

--- finalproject/pages/forms.py
+++ finalproject/pages/forms.py
@@ -5,11 +5,11 @@ class PageForm(forms.ModelForm):
     class Meta:
 
         model = Page
-        fields = ['title', 'content', 'order']
+        fields = ['title', 'content', 'image','order']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
-            'order': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Orden'})
+            'order': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Orden'}),
         }
         labels = {
             'title': '', 'order': '', 'content': ''
--- finalproject/pages/models.py
+++ finalproject/pages/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
+
+def custom_upload_to(instance, filename):
+    old_instance = Page.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return 'page/' + filename
+
+
 class Page(models.Model):
     title = models.CharField(verbose_name="Título", max_length=200)
     content = RichTextField(verbose_name="Contenido")
     order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+    image = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
 
     class Meta:
         verbose_name = "página"
